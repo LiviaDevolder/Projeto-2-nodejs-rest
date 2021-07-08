fcefe19c6abeda5924f3cdb43ab2a14b6798f3bc
LiviaDevolder <liviadevolder@gmail.com>
2021-07-08 15:20:46 -0300

    Aula 5

--- api/Serializador.js
+++ api/Serializador.js
@@ -67,7 +67,6 @@ class SerializadorFornecedor extends Serializador {
     this.contentType = contentType
     this.camposPublicos = [
       'id',
-      'empresa',
       'categoria'
     ].concat(camposExtras || [])
     this.tagSingular = 'fornecedor'
